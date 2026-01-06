4a158995d5a99f81a560ff16e37dfc72c3079285
keizerberos <keizerberos@hotmail.com>
2026-01-06 22:06:05 +0000

    update

--- libs/adb-socket-server.js
+++ libs/adb-socket-server.js
@@ -462,7 +462,8 @@ class AdbSocketServer {
 				socket.send(Buffer.from(str, "hex"));
 				//setTimeout(() => socket.send(self.hexStringToUint8Array("0A00")), 200);
 				setTimeout(() => { socket.close();  }, 600);
-			});			
+			});
+			
 		}
 		let disconnectRemote = (clientDisconnected)=>{
 			let devicesList = getDeviceRemoteList();
