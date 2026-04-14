5e3614eea4623c2d5c83c5c2bfe4a22f6ba9e960
Jorge Tordoya <keizerberos@hotmail.com>
2026-04-13 21:32:48 -0400

    Native Workers

--- libs/executor-master.js
+++ libs/executor-master.js
@@ -3,7 +3,7 @@ const AdbOcr = require('./adb-ocr');
 const os = require('os');
 const fs = require('fs');
 const { Worker } = require('worker_threads');
-const Piscina = require('piscina');
+//const Piscina = require('piscina');
 const { BroadcastChannel } = require('worker_threads');
 const { resolve } = require('path');
 const bc = new BroadcastChannel('my_secret_channel');
@@ -11,14 +11,14 @@ const bc = new BroadcastChannel('my_secret_channel');
 
 const adbocr = new AdbOcr();		
 const maxWorkers = os.cpus().length;
-const piscina = new Piscina({
+/*const piscina = new Piscina({
   filename: './libs/executor-slave.js',	
   //minThreads: 2,   // Always keep 2 workers ready
   //maxThreads: 16,  // Never grow beyond 16 workers
   maxQueue: 10, // Limit the queue size
   //useAtomics: false // Try setting this to false
 });
-
+*/
 let nodeActions = null;
 let patterns = null;
 let events = {};
@@ -107,8 +107,31 @@ class Executor {
 	}
 	startTaskBatch(_devices, task, cbEnd) {
 		//const worker = new Worker('./worker.js');
-		
-		piscina.run({ type:"startTaskBatch", patterns:patterns, actions:nodeActions, data_devices:_devices, data_task:task }).then(res=>{
+		const worker = new Worker(path.resolve(__dirname, './libs/executor-slave.js'), {
+			workerData: { type:"startTaskBatch", patterns:patterns, actions:nodeActions, data_devices:_devices, data_task:task }// Datos iniciales opcionales
+		});
+		worker.on('message', (msg) => {
+			console.log('Recibido del worker:', msg);
+		});
+		worker.on('message', (message) => {
+			if(message.type=="reportCB"){
+				events["reportCB"].forEach(fn=>fn(message.payload.data));
+			}
+			if(message.type=="send"){
+				events["send"].forEach(fn=>fn(message.payload.data));
+			}
+			if(message.type=="task.progress"){
+				console.log("PROGRESS");
+				events["task.progress"].forEach(fn=>fn(message.payload.deviceId, message.payload.progress));
+			}
+		});
+		worker.on('workerDestroy', () => {
+			console.log('A physical worker thread was terminated and removed from the pool.');
+		});
+		worker.on('error', () => {
+			console.log('A physical worker thread was terminated and removed from the pool.');
+		});
+		/*piscina.run({ type:"startTaskBatch", patterns:patterns, actions:nodeActions, data_devices:_devices, data_task:task }).then(res=>{
 			console.log("-WORKER ENDED");
 		});
 		piscina.on('message', (message) => {
@@ -128,7 +151,7 @@ class Executor {
 		});
 		piscina.on('error', () => {
 			console.log('A physical worker thread was terminated and removed from the pool.');
-		});
+		});*/
 
 	}
 	getWorkers(){
